7a48c3f5731a110665a062c2c143d8fce8d5aab1
leesihyeong17 <dltlgud7901@gmail.com>
2026-04-30 14:51:00 +0900

    fix:category field 상관 없이 post 가능

--- likelion14/posts/serializers.py
+++ likelion14/posts/serializers.py
@@ -1,13 +1,14 @@
 ### Model Serializer case
 
 from rest_framework import serializers
-from .models import Post, Comment # Post 모델과 Comment 모델을 가져옴
+from .models import Post, Comment, Category # Post 모델과 Comment 모델을 가져옴
 
 class PostSerializer(serializers.ModelSerializer):
-
-  class Meta:
-    model = Post    # serializer가 어떤 모델을 기반으로 만들어지는지 >> post
-    fields = "__all__"  # 모델에서 어떤 필드를 가져올지 >> 전체 필드
+    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), many=True, required=False, allow_empty=True)
+    #category 필드를 지정하지 않아도 post 생성 가능하도록 required=False, allow_empty=True 옵션 추가
+    class Meta:
+        model = Post    # serializer가 어떤 모델을 기반으로 만들어지는지 >> post
+        fields = "__all__"  # 모델에서 어떤 필드를 가져올지 >> 전체 필드
 
 class CommentSerializer(serializers.ModelSerializer):
 
